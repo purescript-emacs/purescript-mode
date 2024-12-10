2b96f55c52ceeab08945bae94e424408ddabc258
Konstantin Kharlamov <Hi-Angel@yandex.ru>
2024-12-10 21:41:21 +0300

    Decypher regexp at purescript-indentation-peek-token
    The 204 characters line full of backslashes and parentheses isn't the
    most readable code. Replace it with `rx` expression instead, which
    makes author's intentions much clearer.
    
    As can be checked by evaluation, the resulting regexp is exactly the
    same, barring the "'_" part was moved to the left of [:alnum:].

--- purescript-indentation.el
+++ purescript-indentation.el
@@ -998,7 +998,13 @@ indent the current line. This has to be fixed elsewhere."
 
 (defun purescript-indentation-peek-token ()
   "Return token starting at point."
-  (cond ((looking-at "\\(if\\|then\\|else\\|let\\|in\\|ado\\|mdo\\|rec\\|\\(?:[[:word:]]+\\.\\)*do\\|proc\\|case\\|of\\|where\\|module\\|data\\|type\\|newtype\\|class\\|instance\\)\\([^[:alnum:]'_]\\|$\\)")
+  (cond ((looking-at
+          (rx (group
+               (or "if" "then" "else" "let" "in" "ado" "mdo" "rec"
+                   (seq (0+ (seq (1+ word) ".")) "do")
+                   "proc" "case" "of" "where" "module" "data" "type" "newtype"
+                   "class" "instance"))
+              (group (or (not (any alnum "'_")) eol))))
          (match-string-no-properties 1))
         ((looking-at "[][(){}[,;]")
          (match-string-no-properties 0))
