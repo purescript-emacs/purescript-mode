9412ed0d9092238dc4c4f4d432e94edcd4706fce
Konstantin Kharlamov <Hi-Angel@yandex.ru>
2025-05-10 16:50:57 +0700

    purescript-indent.el: make purescript-indent-mode truly buffer-local
    It was defined with `defvar` and then made into buffer-local
    with (make-variable-buffer-local). The call though was only executed
    once, which is likely an omission.
    
    Fix that and make it truly buffer-local.

--- purescript-indent.el
+++ purescript-indent.el
@@ -1486,9 +1486,8 @@ One indentation cycle is used."
 
 ;;; purescript-indent-mode
 
-(defvar purescript-indent-mode nil
+(defvar-local purescript-indent-mode nil
   "Non-nil if the semi-intelligent PureScript indentation mode is in effect.")
-(make-variable-buffer-local 'purescript-indent-mode)
 
 (defvar purescript-indent-map
   (let ((map (make-sparse-keymap)))
